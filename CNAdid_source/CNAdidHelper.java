8dc0d395db2e1f541317ff8b0119d47bf957efc7
licl <licl@shuzilm.cn>
2019-12-04 14:28:53 +0800

    fix CN AdID problem

--- CNAdid_source/CNAdidHelper.java
+++ CNAdid_source/CNAdidHelper.java
@@ -9,15 +9,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-/****************************
- * Created by lchenglan
- * on 2019/11/13
- ****************************
- */
 public class CNAdidHelper {
 
   private String TAG = "CNAdidHelper";
-  private String mPath = "/sdcard/Android/ZHVzY2Lk";
 
   private CNAdidHelper() {
   }
@@ -30,37 +24,58 @@ public class CNAdidHelper {
     private static final CNAdidHelper instance = new CNAdidHelper();
   }
 
-  public String readCNAdid() {
-    String content = "";
-    File file = new File(mPath);
-    if (file.isDirectory() || !file.isFile()) {
-      Log.e(TAG, "The File doesn't not exist.");
-      mPath = "/sdcard/Android/Data/System/local/.ZHVzY2Lk";
-      readCNAdid();
+  public String readCNAdid(Context ctx) {
+    String adid = getCNAdID1(ctx);
+    if (adid != null) {
+      return adid;
     }
-    else {
-      try {
-        InputStream instream = new FileInputStream(file);
-        if (instream != null) {
-          InputStreamReader inputreader = new InputStreamReader(instream);
-          BufferedReader buffreader = new BufferedReader(inputreader);
-          String line;
-          //分行读取
-          while ((line = buffreader.readLine()) != null) {
-            content += line + "\n";
-          }
-          instream.close();
-        }
-      }
-      catch (java.io.FileNotFoundException e) {
+    adid = getCNAdID2(ctx);
+    if (adid != null) {
+      return adid;
+    }
+    adid = getCNAdID3();
+    if (adid != null) {
+      return adid;
+    }
+    return null;
+  }
+  
+  private String getCNAdID1(Context ctx) {
+    String result = null;
+    result = Settings.System.getString(ctx.getContentResolver(), "ZHVzY2Lk");
+    return result;
+  }
+  private String getCNAdID2(Context ctx) {
+    String result = null;
+    String pkgName = ctx.getPackageName();
+    SharedPreferences sp = ctx.getSharedPreferences(pkgName + "_dna", 0);
+    result = sp.getString("ZHVzY2Lk", "NA");
+    if (result.equals("NA")) {
+      return null;
+    }
+    return result;
+  }
+  private String getCNAdID3(Context ctx) {
+    String result = null;
+    String path = "/sdcard/Android/ZHVzY2Lk";
+    try {
+      File file = new File(path);
+      if (file.isDirectory() || !file.isFile()) {
         Log.e(TAG, "The File doesn't not exist.");
+        return null;
       }
-      catch (IOException e) {
-        Log.e(TAG, e.getMessage());
+      InputStream instream = new FileInputStream(file);
+      InputStreamReader inputreader = new InputStreamReader(instream);
+      BufferedReader buffreader = new BufferedReader(inputreader);
+      String line = buffreader.readLine();
+      if (line != null) {
+        result = line;
       }
+      instream.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    Log.e(TAG, "本地文件读取 公共 mCNADID==" + content.split("\n")[0]);
-    return content.split("\n")[0];
+    return result;
   }
 
 }
